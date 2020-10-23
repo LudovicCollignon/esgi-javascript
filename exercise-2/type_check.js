bec8cf3b79241b175dba45300acae1eb1e0c9a8c
LudovicCollignon <lud.collignon@gmail.com>
2020-10-23 18:17:36 +0200

    type_check_v1

---
+++ exercise-2/type_check.js
@@ -0,0 +1,11 @@
+function type_check_v1(value, type) {
+    console.log(typeof value);
+    console.log(value);
+    if (value === null)
+        return type === "null";
+
+    if (Array.isArray(value))
+        return type === "array";
+
+    return type === typeof value;
+}
