8c1dd07bb58c3222f7089065281e546136328eb4
LudovicCollignon <lud.collignon@gmail.com>
2020-10-23 18:44:10 +0200

    type-check-v2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,6 +1,4 @@
 function type_check_v1(value, type) {
-    console.log(typeof value);
-    console.log(value);
     if (value === null)
         return type === "null";
 
@@ -8,4 +6,19 @@ function type_check_v1(value, type) {
         return type === "array";
 
     return type === typeof value;
+}
+
+
+function type_check_v2(value, conf) {
+    let typeCheck = true;
+    let valueCheck = true;
+    let enumCheck = true;
+
+    if (conf.type !== undefined) typeCheck = type_check_v1(value, conf.type);
+
+    if (conf.value !== undefined) valueCheck == conf.value;
+
+    if (conf.enum !== undefined) enumCheck = conf.enum.includes(value);
+
+    return typeCheck && valueCheck && enumCheck;
 }
